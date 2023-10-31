66ec159f423d55deb6766a70c6f208354bd9304d
nklimova <NKlimova@it-one.ru>
2023-10-31 17:20:53 +0600

    added search selenide wiki test

--- src/test/java/SelenideWikiTest.java
+++ src/test/java/SelenideWikiTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
